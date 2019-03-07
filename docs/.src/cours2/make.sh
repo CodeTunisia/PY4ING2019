dc61a6376f2411c2ff7363de6580309c4188433c
astrax <ahmed.ammar@fst.utm.tn>
2019-03-07 01:01:50 +0100

    ajouter youtube demo

--- docs/.src/cours2/make.sh
+++ docs/.src/cours2/make.sh
@@ -48,26 +48,26 @@ doconce replace '\eqref{myeq}' '(ref{myeq})' $1
 doconce replace '\eqref{mysec:eq:Dudt}' '(ref{mysec:eq:Dudt})' $1
 }
 
-doconce pygmentize $name.do.txt perldoc
-system doconce format pdflatex $name --latex_code_style=pyg-gray $options $opt2 --latex_admon=grayicon --latex_admon_title_no_period --latex_copyright=titlepages --latex_title_layout=std --latex_style=std
-# Auto edits
-# With t4/svmono linewidth has some too large value before \mymainmatter
-# is called, so the box width as linewidth+2mm is wrong, it must be
-# explicitly set to 120mm.
-doconce replace '\setlength{\lstboxwidth}{\linewidth+2mm}' '\setlength{\lstboxwidth}{120mm}' $name.tex # lst
-system doconce replace 'linecolor=black,' 'linecolor=darkblue,' $name.tex
-system doconce subst 'frametitlebackgroundcolor=.*?,' 'frametitlebackgroundcolor=blue!5,' $name.tex
-system doconce replace '\maketitle' '\subtitle{Modeling, Algorithms, Analysis, Programming, and Verification}\maketitle' $name.te
-doconce replace 'texttt{>>>}' 'Verb!>>>!' $name.tex # require fix for latex
-
-doconce replace '10pt]{' '10pt,french]{' $name.tex
-# package [norsk]{label} requires texlive-lang-norwegian package
-doconce subst '% insert custom LaTeX commands...' '\usepackage[french]{babel}\n\n% insert custom LaTeX commands...' $name.tex
-system pdflatex -shell-escape $name
-#system bibtex $name
-system makeindex $name
-pdflatex -shell-escape $name
-pdflatex -shell-escape $name
+# doconce pygmentize $name.do.txt perldoc
+# system doconce format pdflatex $name --latex_code_style=pyg-gray $options $opt2 --latex_admon=grayicon --latex_admon_title_no_period --latex_copyright=titlepages --latex_title_layout=std --latex_style=std
+# # Auto edits
+# # With t4/svmono linewidth has some too large value before \mymainmatter
+# # is called, so the box width as linewidth+2mm is wrong, it must be
+# # explicitly set to 120mm.
+# doconce replace '\setlength{\lstboxwidth}{\linewidth+2mm}' '\setlength{\lstboxwidth}{120mm}' $name.tex # lst
+# system doconce replace 'linecolor=black,' 'linecolor=darkblue,' $name.tex
+# system doconce subst 'frametitlebackgroundcolor=.*?,' 'frametitlebackgroundcolor=blue!5,' $name.tex
+# system doconce replace '\maketitle' '\subtitle{Modeling, Algorithms, Analysis, Programming, and Verification}\maketitle' $name.te
+# doconce replace 'texttt{>>>}' 'Verb!>>>!' $name.tex # require fix for latex
+#
+# doconce replace '10pt]{' '10pt,french]{' $name.tex
+# # package [norsk]{label} requires texlive-lang-norwegian package
+# doconce subst '% insert custom LaTeX commands...' '\usepackage[french]{babel}\n\n% insert custom LaTeX commands...' $name.tex
+# system pdflatex -shell-escape $name
+# #system bibtex $name
+# system makeindex $name
+# pdflatex -shell-escape $name
+# pdflatex -shell-escape $name
 
 # # LaTeX Beamer slides
 # beamertheme=red_shadow
