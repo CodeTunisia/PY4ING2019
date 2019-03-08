2fc87cb586e20f4d72dc5137ec228a51bd1a5639
astrax <ahmed.ammar@fst.utm.tn>
2019-03-09 00:03:08 +0100

    update

--- docs/.src/cours2/scripts/MyProject1/UI2PYconverter.py
+++ docs/.src/cours2/scripts/MyProject1/UI2PYconverter.py
@@ -3,8 +3,8 @@
 from PyQt5 import uic
 # Le module uic de PyQt5, convertit un fichier ui (code XML)
 # en un fichier py (code Python)
-fin = open('UiMainApp.ui', 'r')
-fout = open('UiMainApp.py', 'w')
+fin = open('UiMyApp.ui', 'r')
+fout = open('UiMyApp.py', 'w')
 uic.compileUi(fin, fout, execute=True)
 fin.close()
 fout.close()
